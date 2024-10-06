003a0e5a7316592af9ce2cc3b3e9f7b5183eb9d1
JoseOvej <jiovejero2012@gmail.com>
2024-10-06 15:16:03 -0300

    Update Salud_a_la_mano_App.py

--- Salud_a_la_mano_App.py
+++ Salud_a_la_mano_App.py
@@ -112,7 +112,7 @@ if not direccion:
 ##cantidad = int(input("Ingrese la cantidad de establecimientos a mostrar: "))
 
 # El usuario tiene que definir una distancia máxima para filtrar los establecimientos
-dist_maxima = st.slider('Seleccionar distancia máxima', min_value=0, max_value=50)
+dist_maxima = st.slider('Seleccionar distancia máxima', min_value=0, max_value=100)
 
 if not dist_maxima or dist_maxima == 0:
     st.stop()  # Detener la ejecución aquí
@@ -133,6 +133,7 @@ try:
     opciones = sorted(opciones)
 except TypeError as e:
     #st.error(f"Ocurrió un error: {e}")
+    st.warning("Dirección no encontrada. Ingrese nuevamente el dato.)
     st.stop()
 
 # ---------------- DUDA ----------------
